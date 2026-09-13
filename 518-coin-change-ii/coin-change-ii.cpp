4529e4bb148cdbf85baf407da79ef555ed57ada9
MohitChouhan24 <mohitchouhan088@gmail.com>
2026-09-13 12:07:53 +0530

    Time: 35 ms (42.61%) | Memory: 36.7 MB (50.01%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -13,7 +13,23 @@ public:
         int n = coins.size();
         if(n == 0)return 0;
         if(amount == 0)return 1;
-        vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
-        return solve(coins,n,amount,dp);
+        vector<vector<unsigned int>>dp(n+1,vector<unsigned int>(amount+1,-1));
+        for(int i = 0; i <= n; i++){
+            for(int j = 0; j <= amount; j++){
+                if(j == 0)dp[i][j] = 1;
+                if(i == 0)dp[i][j] = 0;
+            }
+        }
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= amount; j++){
+                if(coins[i-1] > j){
+                    dp[i][j] = dp[i-1][j];
+                }
+                else{
+                    dp[i][j] = dp[i][j-coins[i-1]] + dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][amount];
     }
 };
