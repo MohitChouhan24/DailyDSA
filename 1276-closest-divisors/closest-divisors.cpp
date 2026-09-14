723145617674b54245e702adef6e9b864e95ad7b
MohitChouhan24 <mohitchouhan088@gmail.com>
2026-09-14 14:42:30 +0530

    Time: 3 ms (92.47%) | Memory: 8.2 MB (51.17%) - LeetSync

---
+++ 1276-closest-divisors/closest-divisors.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> closestDivisors(int num) {
+        int num1 = num+1,num2 = num+2,dist = INT_MAX;
+        vector<int>result;
+        for(int i = sqrt(num2); i>= 1; i--){
+            if(num2%i == 0){
+                result = {i,num2/i};
+                dist = num2/i-i;
+                break;
+            }
+        }
+        for(int i = sqrt(num1); i >= 1; i--){
+            if(num1%i == 0){
+                if(num1/i-i < dist){
+                    result = {i,num1/i};
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+};
